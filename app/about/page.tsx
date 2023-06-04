218f24169849491b4e730b35a140da6c3453c015
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-06-04 22:50:41 +0530

    :white_check_mark: update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,7 @@ export default function About() {
   return (
     <main className={styles.main}>
       <h1>About page</h1>
+      <p>Consectetur aute do proident anim veniam nostrud sint esse exercitation sunt deserunt. Veniam aliquip cillum magna irure mollit quis duis adipisicing. Ipsum non officia laboris veniam aute nulla. Exercitation deserunt exercitation esse labore nostrud quis ut dolor in pariatur Lorem. Deserunt velit mollit enim laborum fugiat reprehenderit officia esse culpa cillum. Occaecat aliquip sit ea voluptate reprehenderit sit fugiat nostrud consequat excepteur deserunt. Sunt non cupidatat exercitation commodo duis.</p>
     </main>
   )
 }
